dafae516910b217e9f0df8d72cf49b03137ff44b
vihwan <vihwan@ajou.ac.kr>
2020-02-13 18:00:16 +0900

    Add Other List

--- src/main/Database/UWList.java
+++ src/main/Database/UWList.java
@@ -8,6 +8,12 @@ public class UWList{
     public ArrayList<Item> info(){
         info.add(new Item("001","수호의 엑시안","uw","클라우스","기사"));
         info.add(new Item("002","변하지 않는 마음 에레쿠라","uw","제인","기사"));
+        info.add(new Item("003","용의 잔해 어테인","uw","필롭","기사"));
+        info.add(new Item("004","푸른 빛의 아르페우스","uw","데미아","기사"));
+        info.add(new Item("005","승리의 불길 카프레인","uw","모라","기사"));
+        info.add(new Item("006","심판의 빛 테오리아","uw","리카르도","기사"));
+        info.add(new Item("007","폭풍의 눈, 야나디스","uw","소니아","기사"));
+
 
         return info;
     }
