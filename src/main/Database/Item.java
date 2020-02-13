28ea5c3c905e83e0624a8068856e9e54a1f5edd3
vihwan <vihwan@ajou.ac.kr>
2020-02-13 17:29:55 +0900

    Add ArrayList in Item.java

--- src/main/Database/Item.java
+++ src/main/Database/Item.java
@@ -1,9 +1,18 @@
+import java.util.ArrayList;
+
 public class Item {
 
+    private String type;
+    private String name;
+    private String heroClass;
 
+    public Item(String type, String name, String heroClass) {
+        this.type = type;
+        this.name = name;
+        this.heroClass = heroClass;
+    }
 
-
-
-
-
+    public ArrayList<Item> allInOne = new ArrayList<Item>();
+    public ArrayList<Item> uniqueWeapon = new ArrayList<Item>();
+    public ArrayList<Item> ether = new ArrayList<Item>();
 }
